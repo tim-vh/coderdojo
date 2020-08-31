4922d586786b08e7a0573899c101fd7e06b41479
Tim van Heumen <tim@vhweb.nl>
2020-08-31 09:33:12 +0200

    RocketGame aanpassingen

--- python/raket/RocketGame.py
+++ python/raket/RocketGame.py
@@ -3,15 +3,31 @@ import arcade
 class RocketGame(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
-
+        
         arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
-        self.wall_list = arcade.SpriteList()
-        self.rocket = arcade.Sprite("Sprites/motor-uit.png", 1)
-        self.physics_engine = arcade.PhysicsEnginePlatformer(self.rocket, self.wall_list, 0.1)
 
-        self.rocket.change_x = 1
+        self.wall_list = arcade.SpriteList()
+        
+
+        self.rocket = arcade.Sprite("Sprites/motor-uit.png", 1)
+        self.rocket._set_center_x(0)
+        self.rocket._set_center_y(0)
+        self.rocket._set_scale(0.3)
+        #self.rocket.change_x = 1
+
+        self.physics_engine = arcade.PhysicsEngineSimple(self.rocket, self.wall_list)
+
+        self.view_left = -240
+        self.view_bottom = -180
+
+        #arcade.set_viewport(self.view_left, self.view_bottom)
+        arcade.set_viewport(self.view_left,
+                                SCREEN_WIDTH + self.view_left,
+                                self.view_bottom,
+                                SCREEN_HEIGHT + self.view_bottom)
+
         
 
     def on_draw(self):
@@ -22,11 +38,13 @@ class RocketGame(arcade.Window):
 
     def update(self, delta_time):
         # All the logic to move, and the game logic goes here.
-        
+        self.rocket._set_change_y(self.rocket.change_y - 0.1)
+
+
         self.physics_engine.update()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 360
 game = RocketGame(SCREEN_WIDTH, SCREEN_HEIGHT)
 game.setup()
 arcade.run()
